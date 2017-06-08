85103fe3bb272fd75cb379fe45e760fe415ec5aa
Daniel Slaughter <slaughter.dan23@gmail.com>
2017-06-08 12:38:12 -0700

    deployed on heroku. fixed api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,19 +3,19 @@ class HomeController < ApplicationController
 
     @response_game = HTTParty.get "https://igdbcom-internet-game-database-v1.p.mashape.com/games/?fields=*&limit=10&offset=0&order=popularity:desc:min&search=#{params[:search]}&filter[name][any]=#{params[:search]}",
     headers:{
-    "X-Mashape-Key" => "poCTngFpaQmshjnD6vQA5i2cdFOup1jVegVjsnq38udr619BJH",
+    "X-Mashape-Key" => "#{ENV['my_api_key']}",
     "Accept" => "application/json"
     }
 
     @response_series = HTTParty.get "https://igdbcom-internet-game-database-v1.p.mashape.com/collections/?fields=*&limit=10&search=#{params[:search]}",
     headers:{
-    "X-Mashape-Key" => "poCTngFpaQmshjnD6vQA5i2cdFOup1jVegVjsnq38udr619BJH",
+    "X-Mashape-Key" => "#{ENV['my_api_key']}",
     "Accept" => "application/json"
     }
 
     @response_character = HTTParty.get "https://igdbcom-internet-game-database-v1.p.mashape.com/characters/?fields=*&limit=10&search=#{params[:search]}",
   headers:{
-    "X-Mashape-Key" => "poCTngFpaQmshjnD6vQA5i2cdFOup1jVegVjsnq38udr619BJH",
+    "X-Mashape-Key" => "#{ENV['my_api_key']}",
     "Accept" => "application/json"
   }
 
